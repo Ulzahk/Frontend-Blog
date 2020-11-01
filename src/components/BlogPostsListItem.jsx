2abedf9f61bb14bf226b9b1cb77f4def7511b2bd
Ulzahk <ulzahk@gmail.com>
2020-11-01 09:41:54 -0500

    [UPDATED] added category label in blogpostlist and fixed ellipsis

--- src/components/BlogPostsListItem.jsx
+++ src/components/BlogPostsListItem.jsx
@@ -20,7 +20,6 @@ const BlogPostsListsItem = () => {
         console.error(`${err.name}: ${err.message}`)
       })
   }, [])
-
   return (
     state.map((item) => (
       <Link className='blopostslist__container--link' to={`/blogpost/${item.meta_title}`}>
@@ -31,6 +30,7 @@ const BlogPostsListsItem = () => {
           </div>
           <div className='item__rightside'>
             <h3 className='item__rightside--title'>{item.title}</h3>
+            <p className='item__rightside--category'><label>{item.category}</label></p>
             <p className='item__rightside--main'>{item.main_paragraph}</p>
           </div>
         </div>
