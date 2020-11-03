2ea08e9198f8e7f553080ef5dff3e474d813e7f6
Ulzahk <ulzahk@gmail.com>
2020-11-03 09:34:14 -0500

    [UPDATED] joining list with indent section

--- src/containers/CreateBlogPost.jsx
+++ src/containers/CreateBlogPost.jsx
@@ -121,7 +121,6 @@ const CreateBlogPost = (props) => {
                   '|',
                   'numberedList',
                   'bulletedList',
-                  '|',
                   'indent',
                   'outdent',
                   'link',
